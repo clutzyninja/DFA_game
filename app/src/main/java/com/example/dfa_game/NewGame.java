bb2a3d4641dde93493976b50b2f4d428c6306d6e
Masrik Dahir <dahirma@vcu.edu>
2021-12-11 18:06:15 -0500

    The new system for adding question -> to save app space
    Signed-off-by: Masrik <dahirma@vcu.edu>

--- app/src/main/java/com/example/dfa_game/NewGame.java
+++ app/src/main/java/com/example/dfa_game/NewGame.java
@@ -4,21 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.ContentView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewGame extends AppCompatActivity {
+    ImageView question;
+    Button next;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question1);
-        String[][] database = new String[50][4];
-        database[0][0] = "@drawable/even0s";
+        setContentView(R.layout.activity_question);
+        question = findViewById(R.id.imageView2);
+        next = findViewById(R.id.button);
+
+
+        int[][] database = new int[50][4];
+        database[0][0] = R.drawable.even0s;
+
+
+        question.setImageResource(database[0][0]);
 
 
     } // end onCreate
@@ -26,6 +35,8 @@ public class NewGame extends AppCompatActivity {
     public void questionCorrect (View v) {
         Intent i = new Intent(this,Correct.class);
         startActivity(i);
+        next.callOnClick();
+
     }
     public void questionIncorrect (View v) {
         Intent i = new Intent(this,Incorrect.class);
