4ca831bd476c57a0da19958e65091dfa176085cb
Masrik <dahirma@vcu.edu>
2021-12-12 01:00:03 -0500

    first feature added!
    Signed-off-by: Masrik <dahirma@vcu.edu>

--- app/src/main/java/com/example/dfa_game/MainActivity.java
+++ app/src/main/java/com/example/dfa_game/MainActivity.java
@@ -13,19 +13,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button start, regex2nfa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.start);
+        regex2nfa = findViewById(R.id.regex2nfa);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), NewGame.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        regex2nfa.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Regex2nfa.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 
-    public void launchSettings(View v) {
-        // launch a new activity
+//    public void launchSettings(View v) {
+//        // launch a new activity
+//        Intent i = new Intent(this, NewGame.class); // starts main game
+//        startActivity(i);
+//    }
 
-        Intent i = new Intent(this, NewGame.class); // starts main game
-        startActivity(i);
-    }
+
 
 }
 
