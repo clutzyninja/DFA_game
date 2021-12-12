53bfc6d733c4556d9b7b5ceefa9032d45d5438fc
Masrik <dahirma@vcu.edu>
2021-12-11 20:43:15 -0500

    Major changes in teh Brandon's branch
    Signed-off-by: Masrik <dahirma@vcu.edu>

--- app/src/main/java/com/example/dfa_game/Data.java
+++ app/src/main/java/com/example/dfa_game/Data.java
@@ -1,7 +1,7 @@
 package com.example.dfa_game;
 
 public class Data {
-    Object[][] database = new Object[50][5];
+    Object[][] database = new Object[50][6];
 
     public Data(){
         //        Question 1
@@ -10,6 +10,7 @@ public class Data {
         database[0][2] = "Input string has an odd number of 1's";
         database[0][3] = "Input string has an even number of 0's";
         database[0][4] = "Input string has an odd number of 0's";
+        database[0][5] = "A)";
 
 //        Question 2
         database[1][0] = R.drawable.strings_odd_length;
@@ -17,6 +18,15 @@ public class Data {
         database[1][2] = "Input string has even length";
         database[1][3] = "Only the input string \"ab\"";
         database[1][4] = "Only input strings wontaining sequences of \"ab\"";
+        database[1][5] = "B)";
+
+//        Question 1
+        database[2][0] = R.drawable.even0s;
+        database[2][1] = "Input string has an even number of 1's";
+        database[2][2] = "Input string has an odd number of 1's";
+        database[2][3] = "Input string has an even number of 0's";
+        database[2][4] = "Input string has an odd number of 0's";
+        database[2][5] = "A)";
 
     }
 
--- app/src/main/java/com/example/dfa_game/NewGame.java
+++ app/src/main/java/com/example/dfa_game/NewGame.java
@@ -1,5 +1,6 @@
 package com.example.dfa_game;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class NewGame extends AppCompatActivity {
     ImageView question;
-    Button next;
-    TextView option1, option2, option3, option4;
+    Button next, b_1, b_2, b_3, b_4;
+    TextView option1, option2, option3, option4, que;
     int count = 0;
+    Data data = new Data();
 
 
     @Override
@@ -21,47 +23,69 @@ public class NewGame extends AppCompatActivity {
         setContentView(R.layout.activity_question);
 
         question = findViewById(R.id.imageView2);
+        que = findViewById(R.id.que);
         next = findViewById(R.id.button);
+
         option1 = findViewById(R.id.textView12);
         option2 = findViewById(R.id.textView11);
         option3 = findViewById(R.id.textView9);
         option4 = findViewById(R.id.textView10);
 
+        b_1 = findViewById(R.id.button5);
+        b_2 = findViewById(R.id.button6);
+        b_3 = findViewById(R.id.button7);
+        b_4 = findViewById(R.id.button8);
 
-        Data data = new Data();
-
-
-        increment(data.database,count);
-
+        make_question(data.database,count);
 
     } // end onCreate
+
 
     public void questionCorrect (View v) {
         Intent i = new Intent(this,Correct.class);
         startActivity(i);
-        next.callOnClick();
+
+        if (count >= 2) {
+            next.setVisibility(View.INVISIBLE);
+        } else {
+            next.callOnClick();
+        }
+
 
     }
+
 
     public void questionIncorrect (View v) {
         Intent i = new Intent(this,Incorrect.class);
         startActivity(i);
     }
 
-    public void question2 (View v) {
-//        setContentView(R.layout.activity_question);
-        Data data = new Data();
+    public void engine (View v) {
         count += 1;
-        increment(data.database, count);
-
+        make_question(data.database, count);
     }
 
-    public void increment(Object[][] database, int count){
-        question.setImageResource((Integer) database[count][0]);
-        option1.setText((database[count][1].toString()));
-        option2.setText((database[count][2].toString()));
-        option3.setText((database[count][3].toString()));
-        option4.setText((database[count][4].toString()));
+    public int make_question(int val) {
+        return val+=1;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void make_question(Object[][] database, int count){
+        try {
+            question.setImageResource((Integer) database[count][0]);
+            option1.setText((database[count][1].toString()));
+            option2.setText((database[count][2].toString()));
+            option3.setText((database[count][3].toString()));
+            option4.setText((database[count][4].toString()));
+            que.setText("Question: " + make_question(count));
+            b_1.setOnClickListener(b_1.getText().toString().equals(data.database[count][5].toString()) ? this::questionCorrect : this::questionIncorrect);
+            b_2.setOnClickListener(b_2.getText().toString().equals(data.database[count][5].toString()) ? this::questionCorrect : this::questionIncorrect);
+            b_3.setOnClickListener(b_3.getText().toString().equals(data.database[count][5].toString()) ? this::questionCorrect : this::questionIncorrect);
+            b_4.setOnClickListener(b_4.getText().toString().equals(data.database[count][5].toString()) ? this::questionCorrect : this::questionIncorrect);
+            next.setOnClickListener(this::engine);
+        }catch(Exception e){
+            next.setVisibility(View.INVISIBLE);
+        }
     }
 
 } // end NewGame
