8b163c4de72696f9b9991537ae73b7ce5657990b
Malcolm Breckenridge <breckenridmj@vcu.edu>
2021-12-13 01:58:21 -0500

    settings (update)

--- app/src/main/java/com/example/dfa_game/Settings.java
+++ app/src/main/java/com/example/dfa_game/Settings.java
@@ -1,10 +1,10 @@
 package com.example.dfa_game;
-import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+    import androidx.appcompat.app.AppCompatActivity;
+    import android.graphics.Bitmap;
+    import android.os.Bundle;
+    import android.webkit.WebSettings;
+    import android.webkit.WebView;
+    import android.webkit.WebViewClient;
 
 
 
